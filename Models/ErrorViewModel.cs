74f5231f128a95c25ee0d6191b1bff0c45895619
Henrywin11 <henrylanc@gmail.com>
2023-10-08 20:39:36 +0100

    db context was added

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,7 +3,9 @@ namespace DataSibTerminal.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+        DateTime DateTime { get; set; }    
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
+using DataSibTerminal.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//this is test db, we gonna remove connection string latter (i promise) 
+builder.Services.AddDbContext<postgresContext>(options => options.UseNpgsql("Server=158.101.194.79;Port=3066;User Id=postgres;Password=3g0rvt3m3;Database=postgres"));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
