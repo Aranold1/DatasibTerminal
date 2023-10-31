1215934c8ef893d65c73342698fff1fe0893d58e
Egor <nasvay2281337.aslan2009@gmail.com>
2023-11-01 01:41:26 +0700

    uncompiled code,add auth ver 0.0

--- Controllers/AuthorizationPageController.cs
+++ Controllers/AuthorizationPageController.cs
@@ -1,6 +1,15 @@
 ﻿using DataSibTerminal.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
 
 namespace DataSibTerminal.Controllers
 {
@@ -26,15 +35,44 @@ namespace DataSibTerminal.Controllers
             {
                 
                 HttpContext.Response.Headers.Add("set-cookie", $"email=email:{user.Email}, password=pswwd:{user.Password}");
-              
-
                 return RedirectToAction("CreateTicket", "TicketCreationPage");
             }
             return View();
         }
-        bool IsLoginAndPasswordValid()
+
+
+       async Task<bool> IsLoginAndPasswordValid(string Email,string Password)
         {
-            return true;
+            string apiUrl = "http://158.101.194.79:5003/api/authorization   "; //url 
+
+            Users dataToSend = new Users
+            {
+                Id = 1,
+                Name = "sanek",
+                Email = "jmohn@exaple.com",
+                Password = "johns_password",
+            };
+
+            using (var httpClient = new HttpClient())
+           {
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var content = new StringContent(JsonConvert.SerializeObject(dataToSend), System.Text.Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Successfully sent data to the web API.");
+                    Console.WriteLine("Server response: " + responseBody);
+                }
+                else
+                {
+                    Console.WriteLine("Error sending data to the web API. Status code: " + response.StatusCode);
+                }
+                return res;
+            }
         }
       
     }
